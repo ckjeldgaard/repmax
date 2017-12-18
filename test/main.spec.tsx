ef4166107f5e4c42618dc1860719daf3c96c41f3
ckjeldgaard <christoffer@sappy.dk>
2017-12-18 16:01:03 +0100

    Testing service worker registration

--- test/main.spec.tsx
+++ test/main.spec.tsx
@@ -5,18 +5,31 @@ const ReactDOM = require('react-dom');
 ReactDOM.render = jest.fn();
 const spy = jest.spyOn(document, 'getElementById');
 
+Object.defineProperty(window.navigator, 'serviceWorker', {
+  value: {
+    register: jest.fn()
+  }, writable: true
+});
+
 import '../src/main';
 import RepMaxApp from '../src/components/repmax-app';
 
 describe('index', () => {
 
-    it('should index correctly', () => {
-        expect(ReactDOM.render).toHaveBeenCalledTimes(1);
-        expect(ReactDOM.render).toHaveBeenCalledWith(<RepMaxApp />, null);
-    });
+  it('should index correctly', () => {
+    expect(ReactDOM.render).toHaveBeenCalledTimes(1);
+    expect(ReactDOM.render).toHaveBeenCalledWith(<RepMaxApp/>, null);
+  });
 
-    it('should find the correct container', () => {
-        expect(spy).toHaveBeenCalledTimes(1);
-        expect(spy).toHaveBeenCalledWith('app-main');
-    });
+  it('should find the correct container', () => {
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith('app-main');
+  });
+
+  it('should register a service worker', () => {
+    const navigatorSpy = jest.spyOn(window.navigator.serviceWorker, 'register');
+    expect(navigatorSpy).toHaveBeenCalled();
+    expect(navigatorSpy).toHaveBeenCalledWith('service-worker.js');
+  });
+
 });
