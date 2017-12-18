16d7cfbfffe5097626305cecfe3af126df0e77c2
ckjeldgaard <christoffer@sappy.dk>
2017-12-18 10:12:03 +0100

    Increased stryker timeout to 10,000 ms.

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,22 +1,23 @@
 module.exports = function (config) {
-    config.set({
-        files: [
-            "src/**/*.tsx",
-            "src/**/*.ts",
-            "test/**/*.tsx",
-            "test/**/*.ts"
-        ],
-        testRunner: "jest",
-        reporter: ["html", "clear-text", "progress"],
-        coverageAnalysis: "off",
-        mutate: ["src/**/*.tsx", "src/**/*.ts"],
-        mutator: 'typescript',
-        transpilers: ["typescript"],
-        tsconfigFile: "tsconfig.json",
-        thresholds: {
-            high: 80,
-            low: 60,
-            break: 80, // Fail if mutation score < 80
-        }
-    });
+  config.set({
+    files: [
+      "src/**/*.tsx",
+      "src/**/*.ts",
+      "test/**/*.tsx",
+      "test/**/*.ts"
+    ],
+    testRunner: "jest",
+    reporter: ["html", "clear-text", "progress"],
+    coverageAnalysis: "off",
+    mutate: ["src/**/*.tsx", "src/**/*.ts"],
+    mutator: 'typescript',
+    transpilers: ["typescript"],
+    tsconfigFile: "tsconfig.json",
+    thresholds: {
+      high: 80,
+      low: 60,
+      break: 80, // Fail if mutation score < 80
+    },
+    timeoutMs: 10000
+  });
 };
